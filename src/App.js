827cf3f80c04053c1a01ab859f087ca658012c5a
Kevin Patel <patelkevin.4874@gmail.com>
2024-11-16 18:45:17 +0530

    pratice
    practice on component ,export component and import component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 // import TextForm from './component/TextForm';
-import './App.css';
+
 import TextForm2 from './component/TextFormTwo';
 
 import Navbar from './component/Navbar'
 
-import Footer from'./component/Footer'
+import Footer from './component/Footer'
 
+
+// import Practice from "./component/Practice"
+
+
+import React from "react";
+
+import './App.css';
 
 function App() {
   return (
     <>
 
-      
-        <Navbar></Navbar>
-        {/* // <TextForm title='Fill the form'></TextForm>  */}
-        <TextForm2 heading="Fill your Text"></TextForm2>
+      {/* <Practice></Practice> */}
+
+      <Navbar></Navbar>
+
+      {/* <TextForm title='Fill the form'></TextForm> */}
+
+      <TextForm2 heading="Fill your Text"></TextForm2>
+
       <Footer></Footer>
+
     </>
   );
 }
---
+++ src/component/Practice.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Practice() {
+    return (
+
+        setTimeout(() => {
+            console.log("practice number 3")
+        }, 6000)
+    )
+}
+
+React()
